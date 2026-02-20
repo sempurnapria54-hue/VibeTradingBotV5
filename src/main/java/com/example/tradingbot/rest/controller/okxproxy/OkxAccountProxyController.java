59bada8915419ffee619312d9205c7839b7688ae
sempurnapria54-hue <sempurnapria54@gmail.com>
2026-02-21 00:11:37 +0300

    Refactor OKX proxy controllers to return raw OkxApiResponse

--- src/main/java/com/example/tradingbot/rest/controller/okxproxy/OkxAccountProxyController.java
+++ src/main/java/com/example/tradingbot/rest/controller/okxproxy/OkxAccountProxyController.java
@@ -4,37 +4,28 @@ import com.example.tradingbot.client.model.okx.BalanceRequest;
 import com.example.tradingbot.client.model.okx.BalanceResponse;
 import com.example.tradingbot.client.model.okx.PositionResponse;
 import com.example.tradingbot.client.model.okx.PositionsRequest;
-import com.example.tradingbot.domain.service.OkxAccountProxyService;
-import com.example.tradingbot.rest.model.okxproxy.RestResponse;
+import com.example.tradingbot.client.model.okx.OkxApiResponse;
+import com.example.tradingbot.client.okx.OkxRestClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/okx/v5/account")
 @RequiredArgsConstructor
 public class OkxAccountProxyController {
 
-    private final OkxAccountProxyService service;
+    private final OkxRestClient okxRestClient;
 
     @GetMapping("/balance")
-    public RestResponse<BalanceResponse> getBalance(BalanceRequest request) {
-        return success(service.getBalance(request));
+    public OkxApiResponse<BalanceResponse> getBalance(BalanceRequest request) {
+        return okxRestClient.getBalance(request);
     }
 
     @GetMapping("/positions")
-    public RestResponse<PositionResponse> getPositions(PositionsRequest request) {
-        return success(service.getPositions(request));
-    }
-
-    private <T> RestResponse<T> success(List<T> data) {
-        RestResponse<T> response = new RestResponse<>();
-        response.setCode("0");
-        response.setMessage("success");
-        response.setData(data);
-        return response;
+    public OkxApiResponse<PositionResponse> getPositions(PositionsRequest request) {
+        return okxRestClient.getPositions(request);
     }
 }
