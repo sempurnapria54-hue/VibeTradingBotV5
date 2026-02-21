b862fac1233673aad2ea7baafde98204f140f1de
sempurnapria54-hue <sempurnapria54@gmail.com>
2026-02-21 13:04:10 +0300

    Add exists query method for candle groups

--- src/main/java/com/example/tradingbot/persistence/repository/CandleGroupRepository.java
+++ src/main/java/com/example/tradingbot/persistence/repository/CandleGroupRepository.java
@@ -16,6 +16,8 @@ public interface CandleGroupRepository extends JpaRepository<CandleGroupEntity, 
 
     Optional<CandleGroupEntity> findByInstrumentIdAndTimeframe(Long instrumentId, String timeframe);
 
+    boolean existsByInstrumentIdAndTimeframe(Long instrumentId, String timeframe);
+
     List<CandleGroupEntity> findAllByInstrumentIdOrderByIdAsc(Long instrumentId);
 
     @Query("""
