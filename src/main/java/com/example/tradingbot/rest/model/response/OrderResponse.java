35e5f319917f8142d1c7ee72c67d2ee5bd4190bf
romanKrdr <romankrasnodar23ru@yandex.ru>
2026-02-22 08:36:17 +0300

    some refactor

--- src/main/java/com/example/tradingbot/rest/model/response/OrderResponse.java
+++ src/main/java/com/example/tradingbot/rest/model/response/OrderResponse.java
@@ -2,13 +2,13 @@ package com.example.tradingbot.rest.model.response;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import java.time.OffsetDateTime;
 
 @Getter
 @Setter
 public class OrderResponse extends Auditable {
 
-    private String instrumentInternalId;
     private String internalId;
     private String externalId;
     private String status;
