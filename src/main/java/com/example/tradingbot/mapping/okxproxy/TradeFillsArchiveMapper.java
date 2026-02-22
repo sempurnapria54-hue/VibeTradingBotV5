21b8effca751412eb2c11a8c7bad136bfeca35d2
romanKrdr <romankrasnodar23ru@yandex.ru>
2026-02-22 07:27:40 +0300

    some refactor

--- src/main/java/com/example/tradingbot/mapping/okxproxy/TradeFillsArchiveMapper.java
+++ src/main/java/com/example/tradingbot/mapping/okxproxy/TradeFillsArchiveMapper.java
@@ -9,12 +9,13 @@ import org.mapstruct.Mapping;
 public interface TradeFillsArchiveMapper {
 
     @Mapping(source = "ts", target = "timestamp")
-    @Mapping(source = "msg", target = "message")
+    @Mapping(source = "code", target = "externalStatusCode")
+    @Mapping(source = "msg", target = "externalStatusMessage")
     ExchangeTradeFillsArchive clientToDomain(TradeFillsArchiveResponse source);
 
     @Mapping(source = "timestamp", target = "ts")
-    @Mapping(source = "message", target = "msg")
+    @Mapping(source = "externalStatusCode", target = "code")
+    @Mapping(source = "externalStatusMessage", target = "msg")
     TradeFillsArchiveResponse domainToClient(ExchangeTradeFillsArchive source);
-
 
 }
